5f82fccbf511170ac0d70e151cf160658f111e09
Amr Ehab <amrabdulaziz98@gmail.com>
2022-02-10 18:38:09 +0200

    code style edits

--- blockchain2.py
+++ blockchain2.py
@@ -1,4 +1,3 @@
-from audioop import avg
 from hashlib import sha256
 import json
 import time
@@ -17,8 +16,10 @@ class Block:
     def compute_hash(self):
         block_string = json.dumps(self.__dict__, sort_keys=True)
         return sha256(block_string.encode()).hexdigest()
+
     def __repr__(self):
-        return "index: "+ str(self.index) +" previous_hash " + str(self.previous_hash) +" time_stamp " + str(time.strftime('%H:%M:%S', time.localtime(self.timestamp)))
+        return "index: " + str(self.index) + " previous_hash: " + str(self.previous_hash) + " time_stamp: "\
+             + str(time.strftime('%H:%M:%S', time.localtime(self.timestamp)))
 
 
 class Blockchain:
@@ -28,12 +29,15 @@ class Blockchain:
         self.create_genesis_block()
         self.current_nonce = 0
         self.difficulty = 2
+
     def get_length(self):
         return len(self.chain)
+
     def create_genesis_block(self):
         genesis_block = Block(0, [], time.time(), "0")
         genesis_block.hash = genesis_block.compute_hash()
         self.chain.append(genesis_block)
+
     @property
     def last_block(self):
         return self.chain[-1]
@@ -65,14 +69,15 @@ class Blockchain:
     def add_new_transaction(self, transaction):
         self.unconfirmed_transactions.append(transaction)
 
-
     def change_difficulty(self):
-        if self.get_length() >2:
-            avg = (self.chain[-(1)].timestamp -self.chain[-(2)].timestamp)
-            if avg>1.3:
-                self.difficulty -=1
-            elif avg<0.2:
-                self.difficulty +=1 
+
+        if self.get_length() > 2:
+            avg = (self.chain[-(1)].timestamp - self.chain[-(2)].timestamp)
+
+            if avg > 1.3:
+                self.difficulty -= 1
+            elif avg < 0.2:
+                self.difficulty += 1
 
     def mine(self, hash_percentage):
         if not self.unconfirmed_transactions:
@@ -94,6 +99,7 @@ class Blockchain:
     def create_random_transactions(self, n):
         for i in range(n):
             self.add_new_transaction(random()*10)
+
     def print(self):
         print(self.chain[-1])
 
@@ -101,10 +107,10 @@ class Blockchain:
 ##################################################
 
 x = 0
-while(x !=1 and x!=2):
-    x = int(input("1) blockchain simulation \n2) attack simulation\n"))
+while(x != 1 and x != 2):
+    x = int(input("Select a program:\n1) Blockchain simulation \n2) Attack simulation\n"))
 
-if x==1:
+if x == 1:
     blockchain = Blockchain()
     while True:
         blockchain.create_random_transactions(100)
@@ -113,14 +119,15 @@ if x==1:
             blockchain.mine(100)
         blockchain.change_difficulty()
         blockchain.print()
-elif x==2:
+elif x == 2:
     blockchain_honest = Blockchain()
-    attack_speed =-1
-    while attack_speed>100 or attack_speed<=0:
+    attack_speed = -1
+    while attack_speed > 100 or attack_speed <= 0:
         attack_speed = int(input("Enter the attacker computational power percentage: "))
     print("Attacking...")
     honest_speed = 100 - attack_speed
-    #################initialization####################
+
+    # ################initialization####################
     # start honest chain
     for i in range(3):
         blockchain_honest.create_random_transactions(100)
@@ -130,10 +137,10 @@ elif x==2:
 
     blockchain_attacker = copy.deepcopy(blockchain_honest)
     blockchain_attacker.chain.pop()
-    flag =False
+    flag = False
     while True:
         blockchain_honest.create_random_transactions(100)
-        blockchain_attacker.unconfirmed_transactions = copy.deepcopy(blockchain_honest.unconfirmed_transactions)
+        blockchain_attacker.create_random_transactions(100)
 
         if(attack_speed >= honest_speed):
             blockchain_attacker.mine(attack_speed)
@@ -141,12 +148,10 @@ elif x==2:
         else:
             blockchain_honest.mine(honest_speed)
             blockchain_attacker.mine(attack_speed)
-        
-        if blockchain_honest.get_length() - blockchain_attacker.get_length() == 5 and not flag :
+
+        if blockchain_honest.get_length() - blockchain_attacker.get_length() == 5 and not flag:
             print("Honest chain is 5 blocks longer than the attacker chain.\nAttacker will keep trying...")
-            flag =True
+            flag = True
         if blockchain_attacker.get_length() - blockchain_honest.get_length() >= 5:
             print("Attack succeeded with computing power", attack_speed, "%")
             break
-
-
